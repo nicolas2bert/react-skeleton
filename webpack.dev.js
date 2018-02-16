e70bc220b261c0a5a088e637b97d350ee6c6ebed
Nicolas Humbert <nicolas.humbert@live.fr>
2018-02-16 15:39:28 -0800

    React skeleton

---
+++ webpack.dev.js
@@ -0,0 +1,16 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+const path = require('path');
+
+module.exports = merge(common, {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'public/assets'),
+        host: '127.0.0.1',
+        port: 8181,
+        open: true,
+        historyApiFallback: true,
+        hot: true,
+    },
+});
